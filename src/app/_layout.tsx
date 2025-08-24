30ab70440516a3e3f9186f2e3c6e4567eb16d7f5
SereiNeath Reasey <reasey.sereineath19@kit.edu.kh>
2025-08-25 00:57:03 +0900

    feat: implement shop, categories, and product layouts with navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,25 @@
-import { Stack } from "expo-router"
-
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="home" options={{ title: "Home" }} />
-            <Stack.Screen name="cart" options={{ title: "Cart" }} />
-            <Stack.Screen name="auth" options={{ title: "Auth" }} />
-        </Stack>
-    )
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: false, title: "Shop" }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: true, title: "Categories" }}
+      />
+      <Stack.Screen
+        name="products"
+        options={{ headerShown: true, title: "Products" }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{ presentation: "modal", title: "Shopping Cart" }}
+      />
+      <Stack.Screen name="auth" options={{ headerShown: true }} />
+    </Stack>
+  );
 }
